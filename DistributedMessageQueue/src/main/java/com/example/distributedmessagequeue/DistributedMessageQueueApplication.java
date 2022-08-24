b95abd373adedd23209815ef9f58961cb30e7ac6
aaartorias <rajputshubhendu@gmail.com>
2022-08-24 18:05:41 +0530

    initial commit

---
+++ DistributedMessageQueue/src/main/java/com/example/distributedmessagequeue/DistributedMessageQueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.distributedmessagequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedMessageQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedMessageQueueApplication.class, args);
+    }
+
+}
---
+++ DistributedMessageQueue/src/test/java/com/example/distributedmessagequeue/DistributedMessageQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.distributedmessagequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedMessageQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
